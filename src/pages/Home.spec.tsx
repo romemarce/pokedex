4b36ec4f4b0e7327e99e1f1bc89bd203ff31583c
Marcelo Romero <mrromero@teco.com.ar>
2025-03-07 09:38:23 -0300

    test

--- src/pages/Home.spec.tsx
+++ src/pages/Home.spec.tsx
@@ -1,7 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import Home from './Home';
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Home from "./Home";
 
-test('renders welcome text', () => {
-  render(<Home />);
-  expect(screen.getByText('Welcome to Pokedex')).toBeInTheDocument();
+test("renders Home page correctly", () => {
+  render(
+    <MemoryRouter>
+      <Home />
+    </MemoryRouter>
+  );
+
+  expect(
+    screen.getByRole("link", { name: /Go to Pokédex/i })
+  ).toBeInTheDocument();
 });
