efc049a30b02b5d3a866a97a21b8cbf7416c5e4c
Marcelo Romero <mrromero@teco.com.ar>
2025-03-07 09:34:12 -0300

    test

---
+++ src/pages/Home.spec.tsx
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import Home from './Home';
+
+test('renders welcome text', () => {
+  render(<Home />);
+  expect(screen.getByText('Welcome to Pokedex')).toBeInTheDocument();
+});
